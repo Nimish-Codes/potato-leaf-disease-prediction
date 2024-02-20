61c80bb281019b3c1c3258ae23253b856d77d7dc
Nimish Karn <150058194+Nimish-Codes@users.noreply.github.com>
2024-02-20 08:48:16 +0530

    Update potato_leaf_disease_prediction.py

--- potato_leaf_disease_prediction.py
+++ potato_leaf_disease_prediction.py
@@ -15,25 +15,25 @@ def main():
     st.title("Potatoo leaf disease prediction")
     st.warning("upload only unhealthy(Early blight or Late blight) potato leaf image")
     
-  # used model:
-  image_path = st.file_uploader("choose or upload an image of unhealthy potato leaf", type = ["jpg", "jpeg", "png", "webp"])
+    # used model:
+    image_path = st.file_uploader("choose or upload an image of unhealthy potato leaf", type = ["jpg", "jpeg", "png", "webp"])
 
-  if image_path is not None:
+    if image_path is not None:
   
-    img_height, img_width = 150, 150
+      img_height, img_width = 150, 150
   
-    # Prediction function
-    def predict_leaf_disease(image_path):
-      img = tf.keras.preprocessing.image.load_img(image_path, target_size=(img_height, img_width))
-      img_array = tf.keras.preprocessing.image.img_to_array(img)
-      img_array = np.expand_dims(img_array, axis=0) / 255.0
-      prediction = model.predict(img_array)
-      class_labels = ['Early_Blight', 'Healthy', 'Late_Blight']
-      predicted_class = class_labels[np.argmax(prediction)]
-      return predicted_class
+      # Prediction function
+      def predict_leaf_disease(image_path):
+        img = tf.keras.preprocessing.image.load_img(image_path, target_size=(img_height, img_width))
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0) / 255.0
+        prediction = model.predict(img_array)
+        class_labels = ['Early_Blight', 'Healthy', 'Late_Blight']
+        predicted_class = class_labels[np.argmax(prediction)]
+        return predicted_class
     
-    predicted_class = predict_leaf_disease(image_path)
-    st.success("Predicted disease:", predicted_class)
+      predicted_class = predict_leaf_disease(image_path)
+      st.success("Predicted disease:", predicted_class)
 
 if __name__ == '__main__':
   main()
